5dfc55834a8ad95354e81bcbe162a300e2f59dca
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-01-16 17:27:01 -0700

    Adds image model testing and model information

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,6 @@
 class Image < ApplicationRecord
+  validates :url, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :url }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :category }
+  end
 end
