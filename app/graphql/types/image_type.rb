d0156858b8756b3b737017308dfb57b4aae0fbc3
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-01-16 19:58:02 -0700

    Updates image type to use module types

--- app/graphql/types/image_type.rb
+++ app/graphql/types/image_type.rb
@@ -1,6 +1,8 @@
-class Types::Image < GraphQL::Schema::Object
-  field :url, String, null: false
-  field :name, String, null: false
-  field :description, String, null: false
-  field :category, String, null: false
+module Types
+  class ImageType < Types::BaseObject
+    field :url, String, null: false
+    field :name, String, null: false
+    field :description, String, null: false
+    field :category, String, null: false
+  end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,5 @@ module Types
     field :get_image, Types::ImageType, null: false, description: 'Returns a single image by id' do
       argument :id, ID, required: true
     end
-
   end
 end
