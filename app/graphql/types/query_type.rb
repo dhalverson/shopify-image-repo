8a08918248c79d73337118c15688d924af13c37a
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-01-17 19:51:14 -0700

    Adds image search functionality

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,12 +6,25 @@ module Types
 
     field :get_images, [Types::ImageType], null: false, description: 'Returns all images'
 
+    field :search_images, [ImageType], null: false, description: 'Returns images based on keyword' do
+      argument :description, String, required: false
+    end
+
     def get_image(id:)
       Image.find(id)
     end
 
     def get_images
       Image.all
+    end
+
+    def search_images(description: nil)
+      require 'pry'; binding.pry
+      if description
+        Image.where("description ILIKE ?", "%#{description}%").limit(10)
+      else
+        Image.all.limit(10)
+      end
     end
   end
 end
